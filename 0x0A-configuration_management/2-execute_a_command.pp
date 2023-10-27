bfc61c9133e027d43785eb505775c74e8cc04697
Ezejanu <Ezejanu_nno@ymail.com>
2023-10-27 16:51:52 -0700

    fixing task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
 # A manifest that kills a process named killmenow
 
-exec { 'pkill_killmenow_process':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin/',
-  onlyif      => 'pgrep killmenow',
-  refreshonly => true,
+exec { 'pkill killmenow':
+  path => '/usr/bin/'
 }
