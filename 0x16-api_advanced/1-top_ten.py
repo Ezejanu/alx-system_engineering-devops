e72b7ee4af7fe04b1aab6610a73a8b25d9aa675c
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-14 11:55:02 -0800

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9'
+    headers = {'User-Agent': 'RedditTopTen'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        for post in data['data']['children']:
+            print(f"{post['data']['title']}")
+
+    else:
+        print("None")
