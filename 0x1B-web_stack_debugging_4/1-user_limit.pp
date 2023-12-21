5be6ea2d3adca6d789e5ed90088ca9ee463b83f8
Ezejanu <Ezejanu_nno@ymail.com>
2023-12-21 14:15:04 -0800

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# A script to change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
+
+# Increasing hard limit
+exec { 'increase_hard_limit':
+  command => "sed -i '/^holberton hard/s/10/30000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+
+# Increasing soft limit
+exec { 'increase_soft_limit':
+  command => "sed -i '/^holberton soft/s/5/30000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
+}
